e248ee9e8f5897c18ae23fb52ea1acff04de3f99
albingcj <114584909+albingcj@users.noreply.github.com>
2023-07-22 23:45:50 +0530

    ff

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
-var container = $('.horizontal-scroll')
-var appendTo = container.find('.h-inner-scroll')
-var menuItems = appendTo.find('div')
-var paddingSize = 50
+var projectOne = document.getElementById("project1");
+var imgs = projectOne.querySelectorAll(".project");
+var imgLength = imgs.length;
 
-container.scrollTop(paddingSize)
+t = 0;
+var gap = 20;
+var tot = 0;
 
-container.scroll(function () {
-  menuItems.each(function () {
-    var object = $(this)
-    var posLeft = $(this).offset().left
-    var minWidth = $(this).outerWidth(true)
-    var sum = - (minWidth + paddingSize)
-    var containerWidth = minWidth * (menuItems.length - 1)
-    
-    if (posLeft <= sum) {
-      appendTo.append(object)
-    }  else if (posLeft >= containerWidth) {
-      appendTo.prepend(object) 
+// position each img next to each other
+for (var i = 0; i < imgLength; i++) {
+  var img = imgs[i];
+
+  if (i == 0) {
+    img.style.transform = "translate3D(" + tot + "px, -50%, 0)";
+  }
+
+  if (i > 0) {
+    var prev = imgs[i - 1];
+    var prevWidth = prev.clientWidth + gap;
+    tot += prevWidth;
+    img.style.transform = "translate3D(" + tot + "px, -50%, 0)";
+  }
+}
+
+function scroll() {
+  // check all images to see off left
+  for (var i = 0; i < imgs.length; i++) {
+    var img = imgs[i];
+    var distLeft = img.getBoundingClientRect().left;
+
+    // is this img off screen?
+    if (distLeft < 0 - img.clientWidth) {
+      var index = i - 1;
+      if (index < 0) {
+        index = imgLength - 1;
+      }
+
+      // get last element postion
+      var lastPos = imgs[index].style.transform.split(/[()]/)[1];
+      var lastX = lastPos.split(",")[0];
+      newX = parseInt(lastX) + imgs[index].clientWidth + gap;
+
+      // push off screen element to end of divs
+      img.style.transform = "translate3D(" + newX + "px, -50%, 0)";
     }
-    
-    if (container.scrollTop() == 0) {
-      container.scrollTop(paddingSize)
-    }
-  })
-})
+  }
+
+  // lets just move the scroller to the left
+  t -= 1;
+  projectOne.style.transform = "translate3D(" + t + "px, 0, 0)";
+  requestAnimationFrame(scroll);
+}
+requestAnimationFrame(scroll);
