77bccb94fee41b4c6e4bf21c043876fd1562b70d
albingcj <114584909+albingcj@users.noreply.github.com>
2023-07-26 01:33:59 +0530

    listing

---
+++ src/listingscript.js
@@ -0,0 +1,23 @@
+const products = [
+    {
+      name: 'Gloves 2Pink',
+      photo: 'https://hulajnogimicro.pl/media/catalog/product/cache/5/image/9df78eab33525d08d6e5fb8d27136e95/p/n/png-micro_gloves_doodle_dot_2_.png',
+      brand: '2Pink',
+      color: '#EB6CA4'
+    },
+    {
+      name: 'Coach Pink',
+      photo: 'https://res.cloudinary.com/yakir/image/upload/v1578887770/r69yfic0g5ndp7vkv5rb.png',
+      brand: 'Coach',
+      color: '#EFB2AF'
+    },
+    {
+      name: 'Belt',
+      photo: 'https://media2.bulgari.com/f_auto,q_auto/production/dw226090d9/images/images/466646.png',
+      brand: 'Bvlgari',
+      color: '#424242'
+    }
+  ];
+  
+  
+  
