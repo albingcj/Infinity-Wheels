7066c0dcd0d88a1a8df27918dbf3a09bf6e5a01b
albingcj <114584909+albingcj@users.noreply.github.com>
2023-07-25 16:23:05 +0530

    ui fixes

--- script/script.js
+++ script/script.js
@@ -1,1 +1,18 @@
 document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('show')); 
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     // Get the loading indicator element
+//     var loadingIndicator = document.getElementById(".loading-indicator");
+//     // Get the full container element
+//     var fullContainer = document.querySelector(".full");
+  
+//     // Function to hide the loading indicator and display the content
+//     function showContent() {
+//       loadingIndicator.style.display = "none";
+//       fullContainer.style.display = "block";
+//     }
+  
+//     // Show the content after 3 seconds
+//     setTimeout(showContent, 3);
+//   });
